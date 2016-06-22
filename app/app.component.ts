0a0cdaa840b64be3cf6ff5071f8c7f33edf802e8
Oleksandr Ievstratiev <oievstratiev@productengine.com>
2016-06-22 18:43:40 +0300

    Finished first task

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,15 @@ export class Hero {
 
 @Component({
 	selector: 'first',
-	template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
+	template: `
+		<h1>{{title}}</h1>
+		<h2>{{hero.name}} details!</h2>
+		<div><label>id: </label>{{hero.id}}</div>
+		<div>
+			<label>name: </label>
+			<input [(ngModel)]="hero.name" placeholder="hero">
+			</div>
+		`
 })
 
 export class AppComponent { 
